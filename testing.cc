410ca3ce865693b96d16b767f4c9c8e151361eab
Mav Levin <33753158+mavlevin@users.noreply.github.com>
2023-03-13 20:02:38 -0700

    Create testing.cc

---
+++ testing.cc
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stddef.h>
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  if (size > 0 && data[0] == 'H')
+    if (size > 1 && data[1] == 'I')
+       if (size > 2 && data[2] == '!')
+       __builtin_trap();
+  return 0;
+}
+
+// Build test_fuzzer.cc with asan and link against libFuzzer.
+// clang++ -fsanitize=address,fuzzer test_fuzzer.cc
+// Run the fuzzer with no corpus.
+// ./a.out
